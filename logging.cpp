0eeaa9c8b5a243d77c8c3bc7ffcd2f0e8184f9e8
Ismaele <tiados04@gmail.com>
2024-05-19 18:46:25 +0200

    added comments on logging file

--- logging.cpp
+++ logging.cpp
@@ -5,11 +5,11 @@
 #include <stdio.h>
 #include <iostream>
 
-extern std::ofstream fileLog;
+extern std::ofstream fileLog; //l'ho messo in main.cpp
 
 //inserisce i messaggi nel file di log
 void log_messaggio(const std::string& messaggio) {
-    auto now = std::chrono::system_clock::now();
-    auto now_c = std::chrono::system_clock::to_time_t(now);
-    fileLog << std::put_time(std::localtime(&now_c), "%Y-%m-%d %H:%M:%S") << " " << messaggio << std::endl;
+    auto now = std::chrono::system_clock::now(); //tempo corrente
+    auto now_c = std::chrono::system_clock::to_time_t(now); //converto in leggibile
+    fileLog << std::put_time(std::localtime(&now_c), "%Y-%m-%d %H:%M:%S") << " " << messaggio << std::endl; //timeStamp e messaggio nel file di log
 }
