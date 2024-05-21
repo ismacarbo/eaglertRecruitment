f64b9a43a2dbaaf482393472ce726a26b16288dd
Ismaele <tiados04@gmail.com>
2024-05-21 10:52:59 +0200

    updated threadReceiver

--- include/threadReceiver.h
+++ include/threadReceiver.h
@@ -1,12 +1,3 @@
-#ifndef THREAD_RECEIVER_H
-#define THREAD_RECEIVER_H
-
-void start_receiver_thread();
-void stop_receiver_thread();
-int open_can(const char* can_interface);
-int can_receive(char* buffer);
-
-#endif // THREAD_RECEIVER_H
 #ifndef THREADRECEIVER_H
 #define THREADRECEIVER_H
 
