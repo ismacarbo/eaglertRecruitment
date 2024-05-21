db5c052bea3af65f028889c0ec84f9a2de471d59
Ismaele <tiados04@gmail.com>
2024-05-21 10:44:20 +0200

    modifiche

--- include/threadReceiver.h
+++ include/threadReceiver.h
@@ -7,3 +7,25 @@ int open_can(const char* can_interface);
 int can_receive(char* buffer);
 
 #endif // THREAD_RECEIVER_H
+#ifndef THREADRECEIVER_H
+#define THREADRECEIVER_H
+
+#include <thread>
+#include <mutex>
+#include <queue>
+#include <condition_variable>
+#include <atomic>
+#include <string>
+
+// Dichiarazione delle funzioni per gestire il thread di ricezione
+void start_receiver_thread();
+void stop_receiver_thread();
+void can_receiver_thread();
+
+// Dichiarazione delle variabili globali per la coda dei messaggi e la sincronizzazione
+extern std::mutex queue_mutex;
+extern std::condition_variable queue_cv;
+extern std::queue<std::string> message_queue;
+extern std::atomic<bool> isRunning;
+
+#endif // THREADRECEIVER_H
--- src/logging.cpp
+++ src/logging.cpp
@@ -1,32 +1,20 @@
-#include <iostream>
+#include "../include/logging.h"
 #include <fstream>
-#include <string>
-#include <ctime>
-
-// Dichiarazione delle funzioni
-void iniziaLog();
-void stopLog();
-void logMessaggio(const std::string& message);
 
 std::ofstream logFile;
 
 void iniziaLog() {
-    std::string fileName = "log_" + std::to_string(std::time(nullptr)) + ".txt";
-    logFile.open(fileName, std::ios::out);
-    if (logFile.is_open()) {
-        std::cout << "Logging started: " << fileName << std::endl;
-    }
+    logFile.open("log.txt", std::ios::out | std::ios::app);
 }
 
 void stopLog() {
     if (logFile.is_open()) {
         logFile.close();
-        std::cout << "Logging stopped." << std::endl;
     }
 }
 
 void logMessaggio(const std::string& message) {
     if (logFile.is_open()) {
-        logFile << std::time(nullptr) << " " << message << std::endl;
+        logFile << message << std::endl;
     }
 }
