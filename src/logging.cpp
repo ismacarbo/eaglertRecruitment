fa015227e1de986bc87dd772561672f7fff48dcd
Ismaele <tiados04@gmail.com>
2024-05-21 10:46:42 +0200

    fixed bugs on logging files

--- src/logging.cpp
+++ src/logging.cpp
@@ -1,31 +1,26 @@
+#include "../include/logging.h"
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <ctime>
 
-// Dichiarazione delle funzioni
-void iniziaLog();
-void stopLog();
-void logMessaggio(const std::string& message);
-
-std::ofstream logFile;
-
-void iniziaLog() {
-    std::string fileName = "log_" + std::to_string(std::time(nullptr)) + ".txt";
+// Funzione per iniziare il logging
+void iniziaLogLogging() {
+    std::string fileName = "../candump.log";
     logFile.open(fileName, std::ios::out);
     if (logFile.is_open()) {
         std::cout << "Logging started: " << fileName << std::endl;
     }
 }
 
-void stopLog() {
+// Funzione per fermare il logging
+void fineLog() {
     if (logFile.is_open()) {
         logFile.close();
         std::cout << "Logging stopped." << std::endl;
     }
 }
 
-void logMessaggio(const std::string& message) {
+// Funzione per processare e registrare un messaggio nel log
+void processaMessaggio_logging(const std::string& message) {
     if (logFile.is_open()) {
         logFile << std::time(nullptr) << " " << message << std::endl;
     }
