8123a47f1851d0b34642bdb471c1f3710000be45
Ismaele <tiados04@gmail.com>
2024-05-21 10:26:47 +0200

    fixed bugs on threadReceiver.h

--- include/threadReceiver.h
+++ include/threadReceiver.h
@@ -1,22 +1,9 @@
-#ifndef THREADRECEIVER_H
-#define THREADRECEIVER_H
+#ifndef THREAD_RECEIVER_H
+#define THREAD_RECEIVER_H
 
-#include <thread>
-#include <mutex>
-#include <queue>
-#include <condition_variable>
-#include <atomic>
-#include <string>
-
-// Dichiarazione delle funzioni per gestire il thread di ricezione
 void start_receiver_thread();
 void stop_receiver_thread();
-void can_receiver_thread();
+int open_can(const char* can_interface);
+int can_receive(char* buffer);
 
-// Dichiarazione delle variabili globali per la coda dei messaggi e la sincronizzazione
-extern std::mutex queue_mutex;
-extern std::condition_variable queue_cv;
-extern std::queue<std::string> message_queue;
-extern std::atomic<bool> isRunning;
-
-#endif // THREADRECEIVER_H
+#endif // THREAD_RECEIVER_H
